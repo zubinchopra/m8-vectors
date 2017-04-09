94bed0a45454f712809657645235911bc32b8dad
zubinchopra <zubin.msvv@gmail.com>
2017-04-08 18:02:41 -0700

    done

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,27 +1,29 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `first.ten` that has the values 10 through 20 in it (using the : operator)
-
+first.ten <- c(10:20)
 
 # Create a vector `next.ten` that has the values 21 through 30 in it (using the seq operator)
-
+next.ten <- c(21:30)
 
 # Create a vector `all.numbers` by combining the vectors `first.ten` and `next.ten`
-
+all.numbers <- c(first.ten, next.ten)
 
 # Create a variable `eleventh` that is equal to the 11th element in your vector `all.numbers`
-
+eleventh <- all.numbers[11]
 
 # Create a vector `some.numbers` that is equal to the second through the 5th elements of `all.numbers`
-
+some.numbers <- all.numbers[c(2:5)]
 
 # Create a variable `len` that is equal to the length of your vector `all.numbers`
-
+len <- length(all.numbers)
 
 ### Bonus ###
 
 # Create a vector `odd` that holds the odd numbers from 1 to 100
-
+numbers <- (1:100)
+odd <- numbers[numbers %% 2 == 1]
 
 # Using the `all` and `%%` operators, confirm that all of the numbers in your `odd` vector are odd
+all(odd %% 2 == 1)
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,22 +1,33 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-
+marbles <- c("red", "orange", "yellow", "green", "purple", "blue")
 
 # Use the `sample` function to select a single marble
-
+sample(marbles, 1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
-
+MarbleGame <- function(guess){
+  marble <- sample(marbles, 1)
+  return(guess == marble)
+}
 
 # Play the marble game!
-
+MarbleGame("blue")
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
+numberOfTries <- 0
+NumberOfTries <- function(){
+  numberOfTries <- numberOfTries + 1
+  MarbleGame("blue")
+}
+
+NumberOfTries()
 
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
+
