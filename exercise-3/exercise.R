d9197dfea27bd3d7efd6b33f956f72b9bba8bca5
zubinchopra <zubin.msvv@gmail.com>
2017-04-08 18:40:59 -0700

    done

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,33 +1,39 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-marbles <- c("red", "orange", "yellow", "green", "purple", "blue")
+marbles <- c("red", "yellow", "orange", "green", "purple", "blue")
 
 # Use the `sample` function to select a single marble
-sample(marbles, 1)
+selectedMarble <- sample(marbles, 1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
 MarbleGame <- function(guess){
-  marble <- sample(marbles, 1)
-  return(guess == marble)
+  return(guess == selectedMarble)
 }
 
 # Play the marble game!
 MarbleGame("blue")
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
-numberOfTries <- 0
-NumberOfTries <- function(){
+answer <- MarbleGame("blue")
+if(answer == FALSE){
   numberOfTries <- numberOfTries + 1
-  MarbleGame("blue")
+}else{
+  numberOfTries <- 0
 }
-
-NumberOfTries()
-
-
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
+noOfTries <- 0
+i <- noOfTries
+for(i in c(1:1000)){
+  check = MarbleGame("blue")
+  if(check)
+    i <- i + 1
+  else
+    break
+}
+average <- noOfTries / 1000
 
